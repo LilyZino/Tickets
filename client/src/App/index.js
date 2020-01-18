86de07d4e95e169ef88f129324872d64d062fa1b
ShakedHad <shakedh100@gmail.com>
2020-01-18 18:22:09 +0200

    Added side bar

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -7,6 +7,15 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Container from '@material-ui/core/Container';
 import styled from 'styled-components';
+import Fab from '@material-ui/core/Fab';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import AddIcon from '@material-ui/icons/Add';
+import InfoIcon from '@material-ui/icons/Info';
+import ConfirmationNumberIcon from '@material-ui/icons/ConfirmationNumber';
 import {
     BrowserRouter as Router,
     Switch,
@@ -18,8 +27,16 @@ import AddPost from './Components/AddPost';
 import Search from './Components/Search';
 
 const useStyles = makeStyles((theme) => ({
+    list: {
+        width: '250px'
+    },
     menuButton: {
         marginRight: theme.spacing(2),
+    },
+    fab: {
+        position: 'absolute',
+        bottom: theme.spacing(2),
+        right: theme.spacing(2),
     }
 }));
 
@@ -30,11 +47,17 @@ const FullTitle = styled(Typography)`
 export default function ButtonAppBar() {
     const classes = useStyles();
 
+    const [drawerState, setDrawerState] = useState(false);
+
+    const onDrawerClicked = () => {
+        setDrawerState(!drawerState);
+    };
+
     return (
         <div>
             <AppBar position="static">
                 <Toolbar>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={onDrawerClicked}>
                         <MenuIcon />
                     </IconButton>
                     <FullTitle variant="h6" className={classes.title}>
@@ -42,6 +65,18 @@ export default function ButtonAppBar() {
                     </FullTitle>
                 </Toolbar>
             </AppBar>
+            <Drawer open={drawerState} onClose={onDrawerClicked}>
+                <List className={classes.list}>
+                    <ListItem button key="about">
+                        <ListItemIcon><InfoIcon /></ListItemIcon>
+                        <ListItemText primary="About Us" />
+                    </ListItem>
+                    <ListItem button key="myTckets">
+                        <ListItemIcon><ConfirmationNumberIcon /></ListItemIcon>
+                        <ListItemText primary="My Tickets" />
+                    </ListItem>
+                </List>
+            </Drawer>
             <Container maxWidth="md">
                 <Router>
                     <Switch>
@@ -58,6 +93,9 @@ export default function ButtonAppBar() {
                             <Search />
                             <PostsList />
                             <AddPost />
+                            <Fab color="primary" aria-label="add" className={classes.fab}>
+                                <AddIcon />
+                            </Fab>
                         </Route>
                         <Route path="/userProfile">
                             user profile
