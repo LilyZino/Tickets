60133a7d643c15c707ad45016de57c8e13f0df48
Shir <shir.avraham13@gmail.com>
2020-01-18 17:51:27 +0200

    removed css file

--- client/src/App/Components/Search/index.js
+++ client/src/App/Components/Search/index.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import './Search.css';
 
 const useStyles = makeStyles({
     search: {
