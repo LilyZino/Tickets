1b58424d1a5410495f744e32bb628652f4a9adcc
Amit <amit.av206@gmail.com>
2020-01-22 20:44:24 +0200

    block add post if not logged in

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,25 +1,29 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-  console.log("Im here!!!")
-  console.log(req.header)
-  console.log(req.header.console)
-  const token = req.header("x-auth-token");
+  const header  = req.header("Authorization");
+  if(typeof header !== 'undefined') {
+      const bearer = header.split(' ');
+      const token = bearer[1];
+    console.log(bearer.lengh)
+    console.log(typeof header)
+    console.log(header)
+    if (!token) {
+      console.log("No token, authorization denied")
+      return res.status(401).json({
+        msg: "No token, authorization denied"
+      });
+    }
 
-  if (!token) {
-    console.log("No token, authorization denied")
-    return res.status(401).json({
-      msg: "No token, authorization denied"
-    });
-  }
-
-  try {
-    console.log("there is token")
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded.user;
-    next();
-  } catch (error) {
-    console.log("Token is not valid")
-    res.status(401).json({ msg: "Token is not valid" });
+    try {
+      console.log("there is token")
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded.user;
+      next();
+    } catch (error) {
+      console.log("Token is not valid")
+      console.log(error)
+      res.status(401).json({ msg: "Token is not valid" });
+    }
   }
 };
