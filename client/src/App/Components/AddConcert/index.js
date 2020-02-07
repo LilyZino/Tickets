720155c6d1fec5ba228ddf69007e422bbb572fd7
Shir <shir.avraham13@gmail.com>
2020-02-07 22:08:46 +0200

    labeling

--- client/src/App/Components/AddConcert/index.js
+++ client/src/App/Components/AddConcert/index.js
@@ -127,11 +127,10 @@ export default function AddTicket() {
                                         ampm={false}
                                         value={enteredTime}
                                         onChange={setEnteredTime}
-                                        label="Date and time"
                                     />
                                 </MuiPickersUtilsProvider>
                                 <TextField
-                                    label="location"
+                                    label="Location"
                                     value={enteredLocation}
                                     onChange={(event) => {
                                         setEnteredLocation(event.target.value);
