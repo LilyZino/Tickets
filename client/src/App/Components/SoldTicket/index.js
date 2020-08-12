273f0e395525acb3bc2b5a7747c244a202239d39
Unknown <unknown@example.com>
2020-08-12 20:33:07 +0300

    add

--- client/src/App/Components/SoldTicket/index.js
+++ client/src/App/Components/SoldTicket/index.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import moment from 'moment';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import { green, red, grey } from '@material-ui/core/colors';
+import { grey } from '@material-ui/core/colors';
 import ListItemText from '@material-ui/core/ListItemText';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
@@ -79,9 +79,12 @@ export default function SoldTicket(props) {
                 <ListItemText>
                     <Typography variant="h5" component="h2">
                         {concert.artist} - {price}₪
-                        </Typography>
+                    </Typography>
                     <Typography className={classes.pos} color="textSecondary">
                         {`${concert.location}, ${moment(concert.time).format('DD/MM/YYYY HH:mm')}`}
+                    </Typography>
+                    <Typography className={classes.pos} color="textSecondary">
+                        {`Ticket by ${user.name}`}
                     </Typography>
                     {file ? (
                         <Button
@@ -91,7 +94,7 @@ export default function SoldTicket(props) {
                         >
                             <a href={`http://localhost:9000/public/${file}`} download={`${concert.artist}-ticket`}>
                                 Download
-                                </a>
+                            </a>
                         </Button>
                     ) : null}
                 </ListItemText>
